24b6b91129ab1f9dd0f2f1b63fd0618af1434851
heart14 <lwf14@qq.com>
2024-10-29 18:47:17 +0800

    dev

--- src/views/Services.vue
+++ src/views/Services.vue
@@ -1,11 +1,49 @@
 <!-- src/views/Services.vue -->
 <template>
-    <div>
-      <h1>Services</h1>
-      <p>This is the services page.</p>
-    </div>
-  </template>
-  
-  <script setup>
-  </script>
-  
+  <div class="main-container">
+    <el-calendar>
+
+      <template #date-cell="{ data }">
+        <div class="date-cell" @mouseover="onMousehover(data.day)" @mouseleave="onMouseleave">
+          <p :class="data.isSelected ? 'is-selected' : ''">
+            {{ data.day.split('-')[2] }}
+            {{ data.isSelected ? '✔️' : '' }}
+          </p>
+
+          <p>
+            今天是{{ data.day }}
+          </p>
+        </div>
+      </template>
+    </el-calendar>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const hoveredDate = ref({
+  byDate: ''
+})
+
+const onMousehover = (day) => {
+  hoveredDate.value.byDate = day
+  console.log('mouse hover:' + day)
+}
+
+const onMouseleave = () => {
+  hoveredDate.value.byDate = ''
+  console.log('mouse leave')
+}
+
+
+</script>
+<style>
+.is-selected {
+  color: #1989fa;
+}
+
+.main-container {
+  width: 100%;
+}
+</style>
