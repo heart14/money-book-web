bea6051c8d2acd7b0bd9c8418c5b010557234569
heart14 <lwf14@qq.com>
2025-11-20 17:33:41 +0800

    feat: 新增icon图标与文字描述的映射关系字典

---
+++ src/utils/constants/icon.ts
@@ -0,0 +1,9 @@
+/**
+ * 图标字典映射，后台返回数据时，根据描述给数据映射一个图标
+ */
+export const DASHBOARD_ICON: Record<string, string> = {
+  总收入: 'ri:pie-chart-line',
+  总支出: 'ri:group-line',
+  收入笔数: 'ri:fire-line',
+  支出笔数: 'ri:progress-2-line'
+}
